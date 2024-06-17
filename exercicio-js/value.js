48973a0de4cbed67f42b43ecc72e24728933fbf9
Gustavo Willer <107127346+willer22@users.noreply.github.com>
2024-06-17 14:53:02 -0300

    Update value.js

--- exercicio-js/value.js
+++ exercicio-js/value.js
@@ -1,13 +1,13 @@
 function calcularValor(valorBoleto, dataVencimento, percentualJurosDiario) {
-    const hoje = new Date();
-    const vencimento = new Date(dataVencimento);
+    let hoje = new Date();
+    let vencimento = new Date(dataVencimento);
     
-    const diff = hoje.getTime() - vencimento.getTime();
-    const diasAtraso = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let diff = hoje.getTime() - vencimento.getTime();
+    let diasAtraso = Math.floor(diff / (1000 * 60 * 60 * 24));
     
     if (diasAtraso > 0) {
-        const juros = valorBoleto * (percentualJurosDiario / 100) * diasAtraso;
-        const valorAtualizado = valorBoleto + juros;
+        let juros = valorBoleto * (percentualJurosDiario / 100) * diasAtraso;
+        let valorAtualizado = valorBoleto + juros;
         return valorAtualizado.toFixed(2);
     } else {
         return valorBoleto.toFixed(2);
